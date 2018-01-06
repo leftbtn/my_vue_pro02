d2600edf545f18950fb06817da1487a5b08bbca3
[andong] <[524679848@qq.com]>
2018-01-06 17:47:01 +0800

    添加vuex保存状态

---
+++ src/vuex/store.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+
+const state = new Object();
+state.IsLogin = false;
+state.UserInformation = new Object();
+
+
+/********改变使用的方法**********/
+const mutations = new Object();
+//判断用户是否登录
+mutations.UserIsLogin = (data => {
+       if(data == null || data == ""){
+           state.IsLogin = false;
+       }else{
+           state.IsLogin = true;
+       }
+    }
+)
+//保存用户信息
+mutations.SaveUserInformation = (user =>{
+    state.UserInformation.userid = user.userid;
+    state.UserInformation.Account = user.Account;
+    state.NikeName = user.NikeName;
+    state.UserInformation.CreateDateTime = user.CreateDateTime;
+})
+
+
+export default new Vuex.Store({
+    state,mutations
+});
