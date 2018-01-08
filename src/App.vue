69b8f14ff89ada567b84f997c256421e8601f405
[andong] <[524679848@qq.com]>
2018-01-08 17:46:33 +0800

    路由切换添加动画

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <div class="banner-body">
      
       <keep-alive>
+        <transition name="fade" mode="out-in">
         <router-view></router-view>
+        </transition>
       </keep-alive>
     
     </div>
